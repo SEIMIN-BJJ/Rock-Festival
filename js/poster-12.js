275ac08db922f31052c53fc78e2a4e0bc0a90c5a
SEIMIN-BJJ <interkq12@gmail.com>
2023-09-04 23:33:36 +0900

    .

--- js/poster-12.js
+++ js/poster-12.js
@@ -1,52 +1,48 @@
-const prev = document.querySelector('.prev')
-prev.addEventListener('click', function () {
-  let page = '../index.html';
+const prev = document.querySelector(".prev");
+prev.addEventListener("click", function () {
+  let page = "../index.html";
   location.href = page;
 });
 
-const next = document.querySelector('.next')
-next.addEventListener('click', function () {
-  let page = 'https://seimin123.github.io/VER-1/';
+const next = document.querySelector(".next");
+next.addEventListener("click", function () {
+  let page = "https://seimin-bjj.github.io/PortFolio-R/";
   location.href = page;
 });
 
 //--------------------------------------
-const move= prev.children;
+const move = prev.children;
 
-let move1 = [ 
-  {transform: 'translateY(10px)'},
-  {transform: 'translateY(-10px)'},
-  {transform: 'translateY(10px)'}
-  
+let move1 = [
+  { transform: "translateY(10px)" },
+  { transform: "translateY(-10px)" },
+  { transform: "translateY(10px)" },
 ];
 
-let moving1= {
+let moving1 = {
   duration: 950,
-  iterations: Infinity
+  iterations: Infinity,
 };
 
 prev.animate(move1, moving1);
 
 //-----------------------------------
 
+const run = document.querySelector(".next");
+const mover = run.children;
 
-const run = document.querySelector('.next');
-const mover= run.children;
-
-let move2 = [ 
+let move2 = [
   // {transform: 'translateX(-100vw)'},
   // {transform: 'translateX(80vw)'},
   // {transform: 'translateX(-100vw)'}
-  {transform: 'translateY(10px)'},
-  {transform: 'translateY(-10px)'},
-  {transform: 'translateY(10px)'}
-  
+  { transform: "translateY(10px)" },
+  { transform: "translateY(-10px)" },
+  { transform: "translateY(10px)" },
 ];
 
-let moving2= {
+let moving2 = {
   duration: 950,
-  iterations: Infinity
+  iterations: Infinity,
 };
 
 run.animate(move2, moving2);
-
